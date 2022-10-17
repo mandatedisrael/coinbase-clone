e6b8908d865a2a3bf767d41e11d4d82271e70a8f
mandatedisrael <mandatedisrael@gmail.com>
2022-10-17 14:06:42 +0100

    Add a price chart

--- coinbase-blockchain-app/components/Portfolio.js
+++ coinbase-blockchain-app/components/Portfolio.js
@@ -3,11 +3,25 @@ import styled from 'styled-components'
 import {BsThreeDotsVertical} from 'react-icons/bs'
 import { coins } from '../static/coins'
 import Coin from './Coin.js'
+import BalanceChart from './BalanceChart.js'
 
 const Portfolio = () => {
   return (
     <Wrapper>
     <Content>
+        <Chart>
+            <div>
+                <Balance>
+                    <BalanceTitle> Portfolio Balance</BalanceTitle>
+                    <BalanceValue>
+                        {'$'}
+                        46,000
+                    </BalanceValue>
+                </Balance>
+            </div>
+            <BalanceChart />
+        </Chart>
+        
     <PortfolioTable>
         <TableItem>
             <Title>Your Asset</Title>
@@ -51,6 +65,23 @@ const Content = styled.div`
 width: 100%;
 max-width: 1000px;
 padding:2rem 1rem;
+`
+
+const Chart = styled.div`
+border: 1px solid #282b2f;
+padding: 1rem 2rem;   
+`
+const Balance = styled.div``
+
+const BalanceTitle = styled.div`
+color:#8a919e;
+font-size: 0.9rem;
+`
+
+const BalanceValue = styled.div`
+font-size: 1.8rem;
+font-weight: 700;
+margin: 0.5rem 0;
 `
 
 const PortfolioTable = styled.div`
--- coinbase-blockchain-app/components/Promos.js
+++ coinbase-blockchain-app/components/Promos.js
@@ -16,6 +16,9 @@ const Promos = () => {
 
         <OfferCard>
             <Title>Learn and Earn</Title>
+            <Description>Earn up to 2.84% APY on your crypto</Description>
+            <Placeholder />
+            <Additional style={{ color: '#3773f5'}}>Verify identity</Additional>
         </OfferCard>
     </Wrapper>
   )
